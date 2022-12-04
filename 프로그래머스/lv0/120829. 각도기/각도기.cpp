c18d99208fb000311de3c5d01f40aa057a4f9bff
ryeongse25 <60475168+ryeongse25@users.noreply.github.com>
2022-12-04 21:40:29 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+    
+    if (angle == 180) answer = 4;
+    else if (angle > 90) answer = 3;
+    else if (angle == 90) answer = 2;
+    else answer = 1;
+    
+    return answer;
+}
