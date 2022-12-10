c44e2411e48ffeb5af86575bd7d3f88fe2407734
ryeongse25 <60475168+ryeongse25@users.noreply.github.com>
2022-12-11 01:02:43 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    for (int i=1; i<=cipher.size(); i++) {
+        if (i % code == 0) answer += cipher[i-1];
+    }
+    return answer;
+}
