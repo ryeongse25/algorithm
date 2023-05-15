6b36794c950fd870a11ab160792ceec6484d401e
ryeongse25 <60475168+ryeongse25@users.noreply.github.com>
2023-05-15 19:22:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,12 +1,4 @@
 def solution(numbers):
-    numbers = numbers.replace("zero", '0')
-    numbers = numbers.replace("one", '1')
-    numbers = numbers.replace("two", '2')
-    numbers = numbers.replace("three", '3')
-    numbers = numbers.replace("four", '4')
-    numbers = numbers.replace("five", '5')
-    numbers = numbers.replace("six", '6')
-    numbers = numbers.replace("seven", '7')
-    numbers = numbers.replace("eight", '8')
-    numbers = numbers.replace("nine", '9')
+    for i, num in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+        numbers = numbers.replace(num, str(i))
     return int(numbers)
