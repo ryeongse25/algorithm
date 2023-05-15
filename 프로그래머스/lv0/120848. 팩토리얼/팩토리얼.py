f6c92b7d65b4aaae2bc45f8b75b763c83fde049a
ryeongse25 <60475168+ryeongse25@users.noreply.github.com>
2023-05-15 20:06:55 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -4,12 +4,7 @@ def factorial(n):
     return n * factorial(n - 1)
 
 def solution(n):
-    
-    facts = [0] * 11
-    
-    for i in range(1, 11):
-        facts[i] = factorial(i)
-        
-    for i in range(10, 0, -1):
-        if n >= facts[i]:
-            return i
+    i = 10
+    while n < factorial(i):
+        i -= 1
+    return i
