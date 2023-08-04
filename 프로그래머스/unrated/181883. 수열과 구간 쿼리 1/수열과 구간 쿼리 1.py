ab5d0de3c5ff5e7e9e263bd9a79382707db92431
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 21:50:25 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 6.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -1,5 +1,5 @@
 def solution(arr, queries):
-    for query in queries:
-        for i in range(query[0], query[1] + 1):
+    for s, e in queries:
+        for i in range(s, e + 1):
             arr[i] += 1
     return arr
