ed8586ddb1c64bf547f94e751e33f573cb715d02
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 17:58:56 +0900

    [unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,8 +1,2 @@
 def solution(todo_list, finished):
-    answer = []
-    
-    for i in zip(todo_list, finished):
-        if i[1] == False:
-            answer.append(i[0])
-    
-    return answer
+    return [val for i, val in enumerate(todo_list) if not finished[i]]
