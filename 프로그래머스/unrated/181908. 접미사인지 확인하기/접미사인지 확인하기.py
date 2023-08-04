db6f725261705cd3d4c176674f08401ab4d742a3
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 23:32:02 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,3 +1,4 @@
 def solution(my_string, is_suffix):
-    return int(my_string.endswith(is_suffix))
+    # return int(my_string.endswith(is_suffix))
+    return int(my_string[-len(is_suffix):] == is_suffix)
         
