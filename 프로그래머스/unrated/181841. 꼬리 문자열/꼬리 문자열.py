1ab10e51dc23a1bd362f165125120e2b6c799103
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 16:55:48 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,6 +1,2 @@
 def solution(str_list, ex):
-    answer = ''
-    for str in str_list:
-        if ex not in str:
-            answer += str
-    return answer
+    return ''.join([str for str in str_list if ex not in str])
