11237bba95709b1d709150030783cf2e68741004
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 14:41:36 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,7 +1,2 @@
 def solution(my_string, is_prefix):
-    answer_list = []
-    
-    for i in range(len(my_string)):
-        answer_list.append(my_string[:i])
-        
-    return int(is_prefix in answer_list)
+    return int(my_string.startswith(is_prefix))
