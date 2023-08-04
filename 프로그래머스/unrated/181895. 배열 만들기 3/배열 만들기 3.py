38cb22b30dac213b98451df5a648e1af7e1345cb
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-04 19:11:45 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,2 +1,4 @@
 def solution(arr, intervals):
-    return arr[intervals[0][0]:intervals[0][1]+1] + arr[intervals[1][0]:intervals[1][1]+1]
+    s1, e1 = intervals[0]
+    s2, e2 = intervals[1]
+    return arr[s1:e1+1] + arr[s2:e2+1]
