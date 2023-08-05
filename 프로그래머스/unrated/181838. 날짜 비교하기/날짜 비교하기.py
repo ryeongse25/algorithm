a2dbcf89257354955d981bdf0e82a793e2395409
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-05 10:41:15 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,13 +1,11 @@
 def solution(date1, date2):
-    if date1[0] == date2[0]:
-        if date1[1] == date2[1]:
-            if date1[2] == date2[2]:
-                return 0
-            else:
-                return 1 if date1[2] < date2[2] else 0
-        else:
-            return 1 if date1[1] < date2[1] else 0
-    else:
-        return 1 if date1[0] < date2[0] else 0
-        
-    return answer
+    
+#     for i in range(3):
+#         if date1[i] < date2[i]:
+#             return 1
+#         elif date2[i] > date1[i]:
+#             return 0
+    
+#     return 0
+
+    return int(date1 < date2)
