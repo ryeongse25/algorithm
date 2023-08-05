6c1de0a3a359ae7b7c4e99971865312b1467e6c9
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-05 11:59:20 +0900

    [unrated] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
@@ -1,11 +1,4 @@
 def solution(n, slicer, num_list):
     a, b, c = slicer
     
-    if n == 1:
-        return num_list[:b+1]
-    elif n == 2:
-        return num_list[a:]
-    elif n == 3:
-        return num_list[a:b+1]
-    else:
-        return num_list[a:b+1:c]
+    return [num_list[:b+1], num_list[a:], num_list[a:b+1], num_list[a:b+1:c]][n-1]
