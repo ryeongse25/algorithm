ddd47a407399ea7b888819630bb771252a6bf2ce
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-06 14:00:58 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,8 +1,10 @@
 def solution(my_string):
-    alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     answer = [0] * 52
     
     for c in my_string:
-        answer[alphabets.index(c)] += 1
+        if c.isupper():
+            answer[ord(c) - 65] += 1
+        else:
+            answer[ord(c) - 97 + 26] += 1
     
     return answer
