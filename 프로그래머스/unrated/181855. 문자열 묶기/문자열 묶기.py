d89b3810e40378a5dc9e2c007039d7fcdf2baddb
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-06 12:16:08 +0900

    [unrated] Title: 문자열 묶기, Time: 15.10 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -2,9 +2,6 @@ def solution(strArr):
     group_by_length = {}
     
     for str in strArr:
-        if len(str) in group_by_length:
-            group_by_length[len(str)] += 1
-        else:
-            group_by_length[len(str)] = 1
+        group_by_length[len(str)] = group_by_length.get(len(str), 0) + 1
     
     return max(group_by_length.values())
