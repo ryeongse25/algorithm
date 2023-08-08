46c16adecbf1e272f628841a9ede87cee60e6375
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-09 00:22:12 +0900

    [unrated] Title: 배열 만들기 2, Time: 28.81 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -3,11 +3,7 @@ def solution(l, r):
     answer = []
     
     for i in range(l, r + 1):
-        flag = True
-        for j in str(i):
-            if j not in zero_or_five:
-                flag = False
-        if flag:
+        if not set(str(i)) - set(['0', '5']):
             answer.append(i)
             
     return answer if answer else [-1]
