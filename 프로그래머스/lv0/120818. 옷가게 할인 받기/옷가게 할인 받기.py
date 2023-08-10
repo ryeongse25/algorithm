1bccf8bd8cf6045591b03456e62e04d082778538
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-10 16:09:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,11 +1,6 @@
 def solution(price):
-    answer = price
+    sale = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
     
-    if price >= 500000:
-        answer = price * 0.8
-    elif price >= 300000:
-        answer = price * 0.9
-    elif price >= 100000:
-        answer = price * 0.95
-    
-    return int(answer)
+    for key, value in sale.items():
+        if price >= key:
+            return int(price * value)
