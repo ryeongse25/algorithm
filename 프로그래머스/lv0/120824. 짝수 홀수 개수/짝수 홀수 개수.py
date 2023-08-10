1c7e70593443afa0de4c43bcd018d2b767a99e42
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-10 13:13:43 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
     answer = [0, 0]
     for num in num_list:
-        if num % 2:
-            answer[1] += 1
-        else:
-            answer[0] += 1
+        answer[num % 2] += 1
     return answer
