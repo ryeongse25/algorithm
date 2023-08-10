2b100f6c699430f8419b727910ecb6cea6e686c7
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-10 19:44:30 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,2 +1,3 @@
 def solution(cipher, code):
-    return ''.join([c for i, c in enumerate(cipher) if (i + 1) % code == 0])
+    # return ''.join([c for i, c in enumerate(cipher) if (i + 1) % code == 0])
+    return cipher[code-1::code]
