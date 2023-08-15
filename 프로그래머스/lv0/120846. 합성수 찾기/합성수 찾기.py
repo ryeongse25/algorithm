26e94d3fe13892b606df95ba550a0b5e706c6b96
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-15 13:40:07 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -2,13 +2,9 @@ def solution(n):
     answer = 0
     
     for i in range(4, n+1):
-        flag = False
-        
         for j in range(2, i):
             if i % j == 0:
-                flag = True
-                
-        if flag:
-            answer += 1
+                answer += 1
+                break
             
     return answer
