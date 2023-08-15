d66aed3663f877c45a2c781594fe8e2abb0fd3a1
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-08-15 23:01:22 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,7 @@
+def solution(id_pw, db):
+    for id, pw in db:
+        if id_pw[0] == id and id_pw[1] == pw:
+            return "login"
+        elif id_pw[0] == id:
+            return 'wrong pw'
+    return 'fail'
