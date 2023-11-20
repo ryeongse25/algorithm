0e1df53392e7bd1f224f722934a050d49eb32ea5
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-11-20 23:44:48 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,9 +1,5 @@
 def solution(num_list, n):
     answer = []
     for i in range(0, len(num_list), n):
-        tmp = []
-        for j in range(n):
-            tmp.append(num_list[i])
-            i += 1
-        answer.append(tmp)
+        answer.append(num_list[i:i+n])
     return answer
