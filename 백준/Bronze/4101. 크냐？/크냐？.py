f942cf60adb1b3655318124daba7e60415d3b2fb
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-11-24 19:43:54 +0900

    [Bronze V] Title: 크냐?, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,10 @@
+while 1:
+    num1, num2 = map(int, input().split())
+
+    if num1 == 0 and num2 == 0:
+        break
+    
+    if num1 > num2:
+        print('Yes')
+    else:
+        print('No')
