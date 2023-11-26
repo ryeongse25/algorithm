8a0be12a4cc8153b218694d84a57db226a1012dd
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-11-26 09:26:30 +0900

    [Bronze II] Title: 분해합, Time: 1208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,13 +2,10 @@ n = int(input())
 answer = 0
 
 for i in range(n):
-    s = str(i)
-    total = i
-    for num in s:
-        total += int(num)
+    total = sum(map(int, str(i)))
+    total += i
     if total == n:
         answer = i
-    if answer != 0:
         break
 
 print(answer)
