ee28cb0922ff526ef6eab9a0db93d01694be7ba6
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-11-26 10:01:32 +0900

    [Bronze II] Title: 블랙잭, Time: 120 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+answer = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if cards[i] + cards[j] + cards[k] <= m:
+                answer = max(answer, cards[i] + cards[j] + cards[k])
+
+print(answer)
