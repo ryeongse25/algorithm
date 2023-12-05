af1a9e18d2eb35dba8c5462869922f0500ae93a6
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-12-05 23:19:14 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,25 @@
+def solution(polynomial):
+    li = polynomial.split(' + ')
+    answer = ''
+    x = 0
+    c = 0
+    
+    for i in li:
+        if 'x' in i:
+            if len(i) == 1:
+                x += 1
+            else:
+                x += int(i[:len(i) - 1])    
+        else:
+            c += int(i)
+    
+    if x != 0 and x != 1:
+        answer += str(x) + 'x'
+    if x == 1:
+        answer += 'x'
+    if x != 0 and c != 0:
+        answer += ' + '
+    if c != 0:
+        answer += str(c)
+    
+    return answer
