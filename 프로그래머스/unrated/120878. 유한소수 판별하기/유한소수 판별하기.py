34350c25dce7c446242e8a00f880a4947ef41090
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-12-05 21:52:17 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,2 +1,15 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
 def solution(a, b):
-    return 1 if len(str(a/b)) < 12 else 2
+    n = gcd(a, b)
+    b //= n
+    
+    while b % 2 == 0:
+        b //= 2
+    while b % 5 == 0:
+        b //= 5
+    
+    return 1 if b == 1 else 2
