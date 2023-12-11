ed9fba306c558424a30cfc346eaeba3307507657
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-12-11 17:41:11 +0900

    [level 1] Title: 최소직사각형, Time: 4.52 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,6 +1,5 @@
 def solution(sizes):
-    for i in sizes:
-        i.sort()
+    sizes = [sorted(i) for i in sizes]
 
     w = max(sizes, key=lambda x: x[0])[0]
     h = max(sizes, key=lambda x: x[1])[1]
