e4c41738c0c33c76939f38d8d7487eb53cdf1421
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-12-11 21:15:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,23 +1,7 @@
 def solution(s):
-    if 'z' in s:
-        s = s.replace('zero', '0')
-    if 'w' in s:
-        s = s.replace('two', '2')
-    if 'u' in s:
-        s = s.replace('four', '4')
-    if 'x' in s:
-        s = s.replace('six', '6')
-    if 'g' in s:
-        s = s.replace('eight', '8')
-    if 'o' in s:
-        s = s.replace('one', '1')
-    if 'h' in s:
-        s = s.replace('three', '3')
-    if 'f' in s:
-        s = s.replace('five', '5')
-    if 'v' in s:
-        s = s.replace('seven', '7')
-    if 'n' in s:
-        s = s.replace('nine', '9')
-
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i, val in enumerate(numbers):
+        s = s.replace(val, str(i))
+    
     return int(s)
