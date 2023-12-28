1b537b40db446630d988697efe9b2db0b76ccbbc
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2023-12-28 09:04:46 +0900

    [level 1] Title: 시저 암호, Time: 3.46 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -2,19 +2,11 @@ def solution(s, n):
     answer = ''
     
     for i in s:
-        new_ascii = ord(i) + n
-        
         if i == ' ':
             answer += ' '
         elif i.isupper():
-            if new_ascii > 90:
-                answer += chr(new_ascii - 26)
-            else:
-                answer += chr(new_ascii)
+            answer += chr((ord(i) + n - ord('A')) % 26 + ord('A'))
         else:
-            if new_ascii > 122:
-                answer += chr(new_ascii - 26)
-            else:
-                answer += chr(new_ascii)
+            answer += chr((ord(i) + n - ord('a')) % 26 + ord('a'))
             
     return answer
