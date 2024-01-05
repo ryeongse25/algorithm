6a1a4f2fcc5933dbe5167d5986a916fa487da0fc
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-05 16:53:33 +0900

    [Silver II] Title: 최대 힙, Time: 124 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+import sys
+import heapq
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+
+    if num == 0:
+        print(-heapq.heappop(heap)) if len(heap) > 0 else print(0)
+    else:
+        heapq.heappush(heap, -num)
