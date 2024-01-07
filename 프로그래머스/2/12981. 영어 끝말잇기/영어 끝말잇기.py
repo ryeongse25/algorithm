fca2895416c2dcdfe357c8e00bbe9f69b5e66442
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-07 12:17:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,9 @@
 def solution(n, words):
     answer = [0, 0]
-    previous = words[0]
 
     for i in range(1, len(words)):
-        if words[i][0] != previous[-1] or words[i] in words[:i]:
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
             answer = [i % n + 1, i // n + 1]
             break
-        previous = words[i]
 
     return answer
