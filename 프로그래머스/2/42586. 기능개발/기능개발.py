434a43cd647aea04492028bace8e3963626d5264
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-09 12:33:53 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,10 +1,12 @@
+import math
+
 def solution(progresses, speeds):
     t = []
     answer = []
     
     for i, j in zip(progresses, speeds):
         if (100 - i) / j == (100 - i) // j:
-            t.append((100 - i) // j)
+            t.append(math.ceil(100 - i) // j)
         else:
             t.append((100 - i) // j + 1)
 
