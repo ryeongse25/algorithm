ce5dc5945d3e6b73b3da91f1b27afc35157eed8c
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-19 15:18:50 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9320 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+count = [0] * 10001
+
+for _ in range(n):
+    num = int(input())
+    count[num] += 1
+
+for i in range(len(count)):
+    for _ in range(count[i]):
+        print(i)
