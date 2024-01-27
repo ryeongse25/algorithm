ae88d7d87e531ee1f295a5fb12f13cec9ec54343
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-27 23:16:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,21 +1,10 @@
 def solution(n, arr1, arr2):
-    b1 = []
-    b2 = []
     answer = []
     
-    for i in arr1:
-        b1.append(bin(i)[2:].rjust(n, '0'))
-    
-    for i in arr2:
-        b2.append(bin(i)[2:].rjust(n, '0'))
-    
-    for i, j in zip(b1, b2):
-        s = ''
-        for k in range(n):
-            if int(i[k]) or int(j[k]):
-                s += '#'
-            else:
-                s += ' '
+    for i, j in zip(arr1, arr2):
+        s = bin(i | j)[2:].zfill(n)
+        s = s.replace('1', '#')
+        s = s.replace('0', ' ')
         answer.append(s)
-        
+
     return answer
