d0383e0d9517ec9b0250f4532a13f6641b71703d
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-28 17:32:14 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
 def solution(nums):
-    p = {}
-    
-    for i in nums:
-        if i in p:
-            p[i] += 1
-        else:
-            p[i] = 1
-    
-    return min(len(nums) // 2, len(p))
+    return min(len(nums) // 2, len(set(nums)))
