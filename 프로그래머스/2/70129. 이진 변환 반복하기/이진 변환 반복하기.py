347de5a02d7b60d7b16dd988a333c45a6d13c582
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-01-31 17:23:57 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,10 +1,9 @@
 def solution(s):
-    cnt = 0
-    zero = 0
+    iteration, cnt = 0, 0
     
     while s != '1':
-        zero += s.count('0')
+        cnt += s.count('0')
         s = s.replace('0', '')
         s = bin(len(s))[2:]
-        cnt += 1
-    return [cnt, zero]
+        iteration += 1
+    return [iteration, cnt]
