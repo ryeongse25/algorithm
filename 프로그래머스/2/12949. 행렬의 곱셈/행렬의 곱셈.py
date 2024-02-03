5dc70c12f3ec3b0ddc0e18086ae3cb557dde467f
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-02-03 23:17:52 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.57 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,6 +1,9 @@
-import numpy as np
-
 def solution(arr1, arr2):
-    a = np.array(arr1)
-    b = np.array(arr2)
-    return np.dot(a, b).tolist()
+    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+    
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr2)):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+        
+    return answer
