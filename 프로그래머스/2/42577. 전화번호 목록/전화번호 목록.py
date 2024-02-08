ddfb50fcf90a1b598ecc797f6e889cfc5a850f16
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-02-08 15:50:34 +0900

    [level 2] Title: 전화번호 목록, Time: 179.09 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,11 @@
-def solution(pb):
-    pb.sort()
+def solution(phone_book):
+    phone = {i: 1 for i in phone_book}
     
-    for i in range(1, len(pb)):
-        if pb[i].startswith(pb[i - 1]):
+    for num in phone_book:
+        tmp = ''
+        for c in num:
+            tmp += c
+            if tmp in phone and tmp != num:
                 return False
-    
+
     return True
