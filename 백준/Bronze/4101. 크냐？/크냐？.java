c70eb4998ab154607e345112048996df364a7bef
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-10-15 14:54:53 +0900

    [Bronze V] Title: 크냐?, Time: 184 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            if (A == 0 && B == 0) break;
+            if (A > B) System.out.println("Yes");
+            else System.out.println("No");
+        }
+    }
+}
