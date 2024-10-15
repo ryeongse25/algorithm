537fcb5765172a6364f9b8a611f3815aa60d452a
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-10-15 13:56:06 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int n = sc.nextInt();
+            sum += n * n;
+        }
+        System.out.println(sum % 10);
+    }
+}
