4c04dfbfa39e5bcb45d3567f05ccd709d09ae1bb
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-10-15 19:27:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 1504 ms, Memory: 310844 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int max = -1000000;
+        int min = 1000000;
+        
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+        
+        System.out.println(min + " " + max);
+    }
+}
