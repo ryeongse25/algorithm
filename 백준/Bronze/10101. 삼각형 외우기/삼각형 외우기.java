8bb78dae00f7df5b241f7e346b54154e2a4a1524
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-10-16 20:14:22 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 168 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if (a + b + c != 180) System.out.println("Error");
+        else {
+            if (a == b && b == c && c == a) System.out.println("Equilateral");
+            else if (a == b || b == c || c == a) System.out.println("Isosceles");
+            else System.out.println("Scalene");
+        }
+    }
+}
