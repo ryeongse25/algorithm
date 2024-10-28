243c9b8924c9dfc37eedc096b6ea70c08f3eb017
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2024-10-28 14:30:53 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 176 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        System.out.println(2 * d1 + 2 * 3.141592 * d2);
+    }
+}
