2f4a5239dfc9d25f98759765237bb8d86c5ee328
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-08 17:06:57 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 484 ms, Memory: 18420 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(fibonacci(n) + " " + (n - 2));
+    }
+    
+    public static int fibonacci(int n) {
+        if (n == 1 || n == 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
