dc1710209bde7373b1c275192f50515da2b6c156
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-14 12:30:37 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 168 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        
+        for (int i = 0; i < arr.length; i++) {
+            s = s.replace(arr[i], "*");
+        }
+        
+        System.out.println(s.length());
+    }
+}
