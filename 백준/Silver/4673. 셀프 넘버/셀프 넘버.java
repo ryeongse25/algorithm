5971fa05e2109d1420d0ed47fc6750a953e014d9
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-14 12:21:23 +0900

    [Silver V] Title: 셀프 넘버, Time: 132 ms, Memory: 14884 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        boolean[] A = new boolean[10001];
-        for (int i = 0; i < 10001; i++) A[i] = true;
+        int[] A = new int[10001];
         
         for (int i = 1; i < 10000; i++) {
             String num = Integer.toString(i);
@@ -9,11 +8,11 @@ public class Main {
             int sum = i;
             for (int j = 0; j < num.length(); j++) sum += (num.charAt(j) - '0');
             
-            if (sum < 10001) A[sum] = false;
+            if (sum < 10001) A[sum] = 1;
         }
         
         for (int i = 1; i < 10001; i++) {
-            if (A[i]) System.out.println(i);
+            if (A[i] == 0) System.out.println(i);
         }
     }
 }
