21e85e69c8a44c7b6bc562068509334f2ccf54ab
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-26 12:20:07 +0900

    [Silver IV] Title: 보물, Time: 188 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        Integer[] B = new Integer[N];
+        
+        for (int i = 0; i < N; i++) A[i] = sc.nextInt();
+        for (int i = 0; i < N; i++) B[i] = sc.nextInt();
+        
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+        
+        int sum = 0;
+        for (int i = 0; i < N; i++) sum += A[i] * B[i];
+        System.out.println(sum);
+    }
+}
