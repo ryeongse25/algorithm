3c627ea7a74ca22e5b7047a5a959662d7202a52d
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-27 13:12:09 +0900

    [Silver IV] Title: 접미사 배열, Time: 240 ms, Memory: 18588 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String[] arr = new String[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) arr[i] = s.substring(i, s.length());
+        Arrays.sort(arr);
+        
+        for (String temp : arr) System.out.println(temp);
+    }
+}
