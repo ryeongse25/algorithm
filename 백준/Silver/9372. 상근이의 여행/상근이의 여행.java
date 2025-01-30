2e78822440ba632aab69d403f4d24ca490513685
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-01-30 17:46:24 +0900

    [Silver IV] Title: 상근이의 여행, Time: 952 ms, Memory: 235736 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            
+            for (int j = 0; j < M; j++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+            
+            System.out.println(N - 1);
+        }
+    }
+}
