a4d4a83e647adcc710109b093a25c249d441f3bd
KIM SERYEONG <60475168+ryeongse25@users.noreply.github.com>
2025-02-05 15:47:38 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,20 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        int M = 0;
+        int sum = 0;
         int N = sc.nextInt();
-        double[] scores = new double[N];
         
-        for (int i = 0; i < N; i++) scores[i] = sc.nextDouble();
-        Arrays.sort(scores);
-        
-        double sum = 0;
         for (int i = 0; i < N; i++) {
-            scores[i] = scores[i] / scores[N - 1] * 100;
-            sum += scores[i];
+            int num = sc.nextInt();
+            sum += num;
+            if (num > M) M = num;
         }
         
-        System.out.println(sum / N);
+        System.out.println((double) sum * 100 / M / N);
     }
 
 }
